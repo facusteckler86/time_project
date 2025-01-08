4a44ff71af5394c5a36d318f36cb2b41d3bf8525
facusteckler86 <facundomsteckler@gmail.com>
2025-01-07 22:24:20 -0300

    arreglo el css, meto assets, y reviso js

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-const $ = (selector) =>{
-    return document.querySelector(selector);
-}
+const dot = document.querySelector('.dot');
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.min');
+const week = document.querySelector('.week');
 
-const hour = $ ('hour');
-const dot = $ ('dot');
-const min = $ ('min');
-const  week = $ ('week');
-
-let showDot = true;
-
-function upDate(){
+function upDate() {
     showDot = !showDot;
     const now = new Date();
 
-    if(showDot){
+    if (showDot) {
         dot.classList.add('invisible');
-    } else{
+    } else {
         dot.classList.remove('invisible');
     }
 
-    hour.textContent = String(now.getHours())
-    .padStart(2,'0');
-    min.textContent = String(now.getMinutes())
-    .padStart(2,'0');
+    hour.textContent = String(now.getHours()).padStart(2, '0');
+    min.textContent = String(now.getMinutes()).padStart(2, '0');
 
-    Array
-    .from(week.children)
-    .forEach(
-        (ele)=>{
-            ele.classList.remove('activate');
-        }
-    );
-    week
-    .children[now.getDay()]
-    .classList
-    .add('activate');
+    Array.from(week.children).forEach((ele) => {
+        ele.classList.remove('active');
+    });
+    week.children[now.getDay()].classList.add('active');
 }
 
 setInterval(upDate, 500);
